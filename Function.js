2f6165103a0dc6d672cf4dc8b32123af1a309e64
Seth Wheeler <wheelersc@cox.net>
2018-07-03 15:23:01 -0700

    TamperMonkey

--- Function.js
+++ Function.js
@@ -1,45 +1,44 @@
-//This script is intended for use with Google Search Engine through the GreaseMonkey Browser Addon for the Firefox Web Browser - otherwise part of the function will not work
-//because I am not using a https connnection.
-//Written for the ATT App Developer Competition
+//This script is intended for use with Google Search Engine through the Tampermonkey Browser Addon
+//To install and use this script download Tampermonkey, create a new script, and copy/paste this code into the new script
+//Written for the ATT App Developer Competition 2018
 //Written by Seth C. Wheeler
 
 // ==UserScript==
 // @name     Function
-// @version  1
+// @version  0
 // @run-at 	 document-end
-// @require	 https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require  https://code.jquery.com/jquery-2.1.4.min.js
 // @include	 https://www.google.com/search*
 // @include	 http://www.google.com/search*
 // ==/UserScript==
 
+var $ = window.jQuery;
 var UserSearchTerm;
 var commonWords = ["for", "the", "a", "of", "this", "these", "those", "get", "how", "to"];
 var searchedterms = [];
-var runTime = 0;
 var userIP;
 
 $.ajax({
     url: "https://api.ipify.org/?format=json", // Getting user Ip Address
-    async: false,
     dataType: 'json',
+    async: false,
     success: function(data) {
-      userIP = data.ip; // Saving user Ip Address
+        userIP = data.ip; // Saving user Ip Address
     }
 });
 if (window.location.href.slice(0,29) == "https://www.google.com/search" || window.location.href.slice(0,28) == "http://www.google.com/search") // Checking if the user is on Google
 {
-  UserSearchTerm = document.getElementById('lst-ib').value; //lst-ib is the id of the Google Search bar
+    UserSearchTerm = document.getElementById('lst-ib').value; //lst-ib is the id of the Google Search bar
 }
 UserSearchTerm = UserSearchTerm.split(" "); // Spliting the user input into an array of terms
 for (var j = 0; j < UserSearchTerm.length; j++) {
-  if (!commonWords.includes(UserSearchTerm[j].toLowerCase())) //Checking against the commonWords array to remove unimportant term
-  {
-    searchedterms.push([UserSearchTerm[j].toLowerCase()]); // Adding the term(s) to an array of terms stored internally
-  }
+    if (!commonWords.includes(UserSearchTerm[j].toLowerCase())) //Checking against the commonWords array to remove unimportant term
+    {
+        searchedterms.push([UserSearchTerm[j].toLowerCase()]); // Adding the term(s) to an array of terms stored internally
+    }
 }
 $.ajax({
-	url: "http://localhost:3000/?searchedterms=" + searchedterms + "&userIP=" + userIP + "", //sending searched terms and user IP adress to Router.js
-  async: false,
-  type: "POST",
-  dataType:"json"
+    url: "https://attappdevcomp2018.herokuapp.com/savedata?searchedterms=" + searchedterms + "&userIP=" + userIP + "", //sending searched terms and user IP adress to Router.js
+    type: "POST",
+    dataType:"json"
 });
